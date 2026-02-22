2dfd8f59349618ed8426097c942f37835c1abc76
AKA-Zedd <shahatabdalazez@gmail.com>
2026-02-22 17:36:19 +0200

    Add firebase configuration and initialize firestore

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBDL-E_GCH7eRGfF_MdQ3cSuQA5wgPt8Ds",
+  authDomain: "campus-market-d381e.firebaseapp.com",
+  projectId: "campus-market-d381e",
+  storageBucket: "campus-market-d381e.firebasestorage.app",
+  messagingSenderId: "967405445457",
+  appId: "1:967405445457:web:5d2d7321ae6a6c26a53370",
+  measurementId: "G-WZBTMQ4Z70"
+};
