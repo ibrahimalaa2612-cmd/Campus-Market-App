977c2dd60e7f9d389511a2126917fa3aa32906de
AKA-Zedd <shahatabdalazez@gmail.com>
2026-03-04 05:24:33 +0200

    Add auth pages and update layout

--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -1,22 +1,49 @@
+import { useEffect, useState } from 'react';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { Stack, useRouter, useSegments } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebaseConfig';
 import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import 'react-native-reanimated';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<any>(null);
+  const router = useRouter();
+  const segments = useSegments();
+
+  useEffect(() => {
+    const subscriber = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      if (initializing) setInitializing(false);
+    });
+    return subscriber;
+  }, []);
+
+  useEffect(() => {
+    if (initializing) return;
+
+    const inTabsGroup = segments[0] === '(tabs)';
+
+    if (!user && inTabsGroup) {
+      router.replace('/login');
+    } else if (user && segments[0] !== '(tabs)') {
+      router.replace('/(tabs)');
+    }
+  }, [user, initializing, segments]);
+
+  if (initializing) return null;
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="login" />
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="forgot-password" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="change-password" />
+        <Stack.Screen name="Register" />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
